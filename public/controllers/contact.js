dc5a296f8032ec73b14951b1a779fe411a02f1cf
DysonJ <jonathan.a.dyson@gmail.com>
2016-10-18 14:37:01 -0600

    sending animations

--- public/controllers/contact.js
+++ public/controllers/contact.js
@@ -13,6 +13,8 @@ function contactController($http){
     text:'',
   }
 
+  c.sending = false;
+
   function validate(){
     var valid = true;
     if(c.message.name == '' || c.message.email == '' || c.message.text == ''){
@@ -27,8 +29,16 @@ function contactController($http){
       Materialize.toast('Please complete all fields', 10000)
       return
     }
+    c.sending = true;
     $http.post('/mail/send', c.message).then(function(res){
       console.log(res)
+      c.sending = false;
+      Materialize.toast('Your message has been sent!', 10000)
+
+      c.message.name = '';
+      c.message.email = '';
+      c.message.text = '';
+
     })
   }
 }
